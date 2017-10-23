e0e5d43e0d54f689045d89b1774933e11452b99d
VanoverscheldeRuben <vanoverschelde.ruben@gmail.com>
2017-10-23 10:53:17 +0200

    Added an explanation for the -h option in the help screen

--- help.c
+++ help.c
@@ -3,9 +3,16 @@
 void displayHelpScreen() {
 	puts("Usage: rpccracker [OPTIONS...] [-u userlist] [-p passwordlist] [-i ipaddress]");
 	puts("");
+	
+	puts("Minimal main usage options");
 	puts("\t-u\t\tpath to the file that contains all the usernames to enumerate");
 	puts("\t-p\t\tpath to the password table");
 	puts("\t-i\t\tip address of the target");
 	puts("");
+	
+	puts("Help options");
+	puts("\t-h\t\tdisplay help screen (this one)");
+	puts("");
+	
 	puts("");
 }
